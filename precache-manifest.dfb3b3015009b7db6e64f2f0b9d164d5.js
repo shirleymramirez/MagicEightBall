1ce295a707384d72f29f1618bf87fd5c11da88ca
unknown <shirley.mramirez@yahoo.com>
2018-11-09 13:26:07 -0700

    Updates

---
+++ precache-manifest.dfb3b3015009b7db6e64f2f0b9d164d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "440d2c5a5186183c428c",
+    "url": "/MagicEightBall/static/js/runtime~main.440d2c5a.js"
+  },
+  {
+    "revision": "f49192fe2b83373ac235",
+    "url": "/MagicEightBall/static/js/main.f49192fe.chunk.js"
+  },
+  {
+    "revision": "0b13b4885e8ccff8ae99",
+    "url": "/MagicEightBall/static/js/1.0b13b488.chunk.js"
+  },
+  {
+    "revision": "f49192fe2b83373ac235",
+    "url": "/MagicEightBall/static/css/main.70a2785a.chunk.css"
+  },
+  {
+    "revision": "44d2cd7f8b80e45399506c3bad161138",
+    "url": "/MagicEightBall/index.html"
+  }
+];
